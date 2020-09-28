5e6bda6fd9313e1d4667cff23b6aefa3656ebf24
Niklas Treml <treml.niklas@gmail.com>
2020-09-28 11:44:42 +0200

    added comments

--- install-new.sh
+++ install-new.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+echo "######################  Getting Updates  ######################"
+sudo apt-get update -y && sudo apt-get upgrade -y
 
-apt-get update
-sudo apt-get install curl -y
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+echo "######################  Getting apt packages  ######################"
 xargs -a apt-packages.txt sudo apt-get install -y
 
+echo "######################  Getting NodeJS  ######################"
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+echo "######################  Getting Snap Packages  ######################"
 sudo snap install code --classic
 sudo snap install spotify --classic
 sudo snap install discord --classic
 
+echo "######################  Getting Python Packages  ######################"
 pip3 install -r requirements.txt
 
+echo "######################  Getting Docker  ######################"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
