45b87c0aa773ce2d9169887092ddc7222d51fc15
Niklas Treml <treml.niklas@gmail.com>
2020-09-28 11:09:23 +0200

    chagned a few things around

--- install-new.sh
+++ install-new.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "Updating packages"
 apt-get update
+echo "Getting curl"
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 xargs -a apt-packages.txt sudo apt-get install -y
-xargs -a snap-packages.txt sudo snap install --classic
+
+sudo snap install code --classic
+sudo snap install spotify --classic
+sudo snap install discord --classic
+
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
