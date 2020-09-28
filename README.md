cbeae1282c8b99cfaae0feeb0f62ccc215d8faaa
Niklas Treml <treml.niklas@gmail.com>
2020-09-28 14:55:46 +0200

    created a proper readme

--- README.md
+++ README.md
@@ -4,4 +4,55 @@ Installs all the software I need.
 
 Use this oneliner to install everything:
 
-`wget https://raw.githubusercontent.com/NiklasTreml/setup-script/master/fresh-setup.sh && sudo chmod a+x fresh-setup.sh && sudo ./fresh-setup.sh`
+`wget https://raw.githubusercontent.com/NiklasTreml/setup-script/master/fresh-setup.sh && sudo chmod 766 fresh-setup.sh && sudo ./fresh-setup.sh`
+
+or with curl
+
+`curl https://raw.githubusercontent.com/NiklasTreml/setup-script/master/fresh-setup.sh -o fresh-setup.sh && sudo chmod 766 fresh-setup.sh && sudo ./fresh-setup.sh`
+
+# Summary of what this script will do:
+
+### Software Installs
+
+- apt-get update and apt-get upgrade as well as apt update
+- build-essential
+- snap
+- git
+- wget
+- curl
+- vim
+- screen
+- python3.8
+- python3 pip
+- NodeJS
+- VS Code
+- Spotify
+- Discord
+### Python Packages
+- python-dotenv
+- matplotlib
+- numpy
+- pandas
+- requests
+- scikit-learn
+- seaborn
+- tensorflow 2
+- jupyter
+### npm Packages
+- express
+- axios
+- concurrently
+- fetch
+- r2
+- socket.io
+- form-data
+- dotenv
+- brain.js
+- p5
+- ml5
+###Miscellaneous
+- ZSH
+- Oh my ZSH
+- Docker
+- Adds the executing user to group `docker`
+- git config --global credential.helper store
--- fresh-setup.sh
+++ fresh-setup.sh
@@ -1,36 +1,42 @@
 #!/bin/sh
-
-
+#Use this oneliner to download and install the script
+#wget https://raw.githubusercontent.com/NiklasTreml/setup-script/master/fresh-setup.sh && sudo chmod a+x fresh-setup.sh && sudo ./fresh-setup.sh
+#or with curl
+#curl https://raw.githubusercontent.com/NiklasTreml/setup-script/master/fresh-setup.sh -o fresh-setup.sh && sudo chmod a+x fresh-setup.sh && sudo ./fresh-setup.sh
 
 echo "######################  Getting Updates  ######################"
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt update
+apt-get update -y && apt-get upgrade -y
+apt update
 
 echo "######################  Getting NodeJS  ######################"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | -E bash -
 
 
 echo "######################  Getting apt packages  ######################"
-sudo apt-get install snapd -y
-sudo apt-get install git-all -y
-sudo apt-get install wget -y
-sudo apt-get install vim -y
-sudo apt-get install screen -y
-sudo apt-get install python3.8 -y
-sudo apt-get install python3-pip -y
-sudo apt-get install nodejs -y
+apt-get install snapd -y
+apt-get install git-all -y
+apt-get install wget -y
+apt-get install curl -y
+apt-get install vim -y
+apt-get install screen -y
+apt-get install python3.8 -y
+apt-get install python3-pip -y
+apt-get install nodejs -y
 
 
 
 echo "######################  Getting Snap Packages  ######################"
-sudo snap install code --classic
-sudo snap install spotify --classic
-sudo snap install discord --classic
+snap install code --classic
+snap install spotify --classic
+snap install discord --classic
 
 echo "######################  Getting Python Packages  ######################"
-sudo pip3 install python-dotenv \
-flask matplotlib \
-numpy pandas requests \
+pip3 install python-dotenv \
+flask \
+matplotlib \
+numpy \
+pandas \
+requests \
 scikit-learn \
 seaborn \
 tensorflow \
@@ -38,21 +44,21 @@ jupyter                                 #using this instead of the requirements 
 
 
 echo "######################  Getting NPM Packages  ######################"
-sudo apt-get install -y build-essential
-sudo npm i express axios concurrently fetch r2 socket.io form-data dotenv brain.js p5 ml5
+apt-get install -y build-essential
+npm i express axios concurrently fetch r2 socket.io form-data dotenv brain.js p5 ml5
 
 echo "######################  Doing git stuff  ######################"
 git config --global credential.helper store
 
 echo "######################  Getting Docker  ######################"
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker $USER
+sh get-docker.sh
+usermod -aG docker $USER
 
 echo "######################  Install Oh-my-ZSH  ######################"
-sudo apt install zsh -y
-sudo chsh -s $(which zsh)
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt install zsh -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "######################  Setting up Aliases  ######################"
 alias ll="ls -lah"
