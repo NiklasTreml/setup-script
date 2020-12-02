fea24600b5f8916e5f62c984bd10498a81052aa8
niklastreml <treml.niklas@gmail.com>
2020-12-02 20:12:31 +0100

    removed gitignore

--- helper.py
+++
@@ -1,5 +0,0 @@
-l = "- ZSH- Oh my ZSH- Docker- Creates and adds a user to the group `docker`- git config --global credential.helper stor".split("- ")
-l = l[1:]
-l = sorted(l, key=str.casefold)
-for i in l:
-    print("-",i)
