42cc134f5605093612f3ea71995a5fd4a8f8a049
niklastreml <treml.niklas@gmail.com>
2020-12-02 20:06:55 +0100

    sorted npm list

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-l = "- python-dotenv- matplotlib- numpy- pandas- requests- scikit-learn- seaborn- tensorflow 2- jupyter".split("- ")
+l = "- express- axios- concurrently- fetch- r2- socket.io- form-data- dotenv- brain.js- p5- ml5".split("- ")
 l = l[1:]
 l = sorted(l, key=str.casefold)
 for i in l:
